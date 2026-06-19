cab0a54db5c08a29906aec6bcdf5f75d73347816
Jeremiah Heilman <jeremiah.heilman@gmail.com>
2026-06-18 21:37:32 -0500

    -

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,9 @@
 #include "../lib/TofGestures/Test_TofGestures.hpp"
 #endif
 
+#define DISTACE_SENSORS 1
+
+
 // LED SETUP
 #define NUM_LEDS 50
 #define DATA_PIN 4
@@ -20,6 +23,7 @@
 CRGB leds[NUM_LEDS];
 uint8_t num_segments = 11;
 uint8_t led_segments[] = {1, 2, 3, 4, 4, 6, 7, 6, 7, 6, 4}; //segment 5, led 3 is missing
+uint16_t rainbow_counter = 0;
 
 
 // VL53L0X SENSOR SETUP
@@ -60,42 +64,43 @@ void setup() {
   FastLED.show();
   Serial.println("FastLED initialized.");
 
-  // Disable/reset all sensors by driving their XSHUT pins low.
-  for (uint8_t i = 0; i < SENSOR_COUNT; i++)  {
-    pinMode(xshutPins[i], OUTPUT);
-    digitalWrite(xshutPins[i], LOW);
-  }
-  delay(100);
-
   // Enable, initialize, and start each sensor, one by one.
-  for (uint8_t i = 0; i < SENSOR_COUNT; i++)
-  {
-    // Stop driving this sensor's XSHUT low. This should allow the carrier
-    // board to pull it high. (We do NOT want to drive XSHUT high since it is
-    // not level shifted.) Then wait a bit for the sensor to start up.
-    pinMode(xshutPins[i], INPUT);
-    delay(10);
-    sensors[i].setTimeout(500);
-    if (!sensors[i].init()) {
-      Serial.print("Failed to detect and initialize sensor ");
-      Serial.print(i);
-      Serial.print("Halting.");
-      while (1);
+  if (DISTACE_SENSORS) {
+      // Disable/reset all sensors by driving their XSHUT pins low.
+    for (uint8_t i = 0; i < SENSOR_COUNT; i++)  {
+      pinMode(xshutPins[i], OUTPUT);
+      digitalWrite(xshutPins[i], LOW);
     }
-    Serial.print("Initializing sensor  ");
-    Serial.println(i);
-    sensors[i].setAddress(0x2A + i);
-    sensors[i].setTimeout(update_delay/2);
+    delay(100);
+    for (uint8_t i = 0; i < SENSOR_COUNT; i++)
+    {
+      // Stop driving this sensor's XSHUT low. This should allow the carrier
+      // board to pull it high. (We do NOT want to drive XSHUT high since it is
+      // not level shifted.) Then wait a bit for the sensor to start up.
+      pinMode(xshutPins[i], INPUT);
+      delay(10);
+      sensors[i].setTimeout(500);
+      if (!sensors[i].init()) {
+        Serial.print("Failed to detect and initialize sensor ");
+        Serial.print(i);
+        Serial.print("Halting.");
+        while (1);
+      }
+      Serial.print("Initializing sensor  ");
+      Serial.println(i);
+      sensors[i].setAddress(0x2A + i);
+      sensors[i].setTimeout(update_delay/2);
+    }
+    for (uint8_t i = 0; i < SENSOR_COUNT; i++) {
+      Serial.print("Sensor "); Serial.print(i); Serial.println(":"); 
+      Serial.print("Measurement timing budget: ");
+      Serial.println(sensors[i].getMeasurementTimingBudget());
+      Serial.print("Timeout: ");
+      Serial.println(sensors[i].getTimeout());
+      sensors[i].startContinuous(update_delay);
+    }
+    Serial.println("VL53 sensors initialized.");
   }
-  for (uint8_t i = 0; i < SENSOR_COUNT; i++) {
-    Serial.print("Sensor "); Serial.print(i); Serial.println(":"); 
-    Serial.print("Measurement timing budget: ");
-    Serial.println(sensors[i].getMeasurementTimingBudget());
-    Serial.print("Timeout: ");
-    Serial.println(sensors[i].getTimeout());
-    sensors[i].startContinuous(update_delay);
-  }
-  Serial.println("VL53 sensors initialized.");
 
   for (uint8_t i = 0; i < SENSOR_COUNT; i++) {
     
@@ -115,11 +120,13 @@ void loop() {
     Taps[i].update(distances[i]);
   }
 
-  // for (uint8_t i = 0; i < SENSOR_COUNT; i++)  {
-  //   Serial.print(distances[i]);
-  //   Serial.print('\t');
-  // }
-  // Serial.println();
+  if (DISTACE_SENSORS){
+    for (uint8_t i = 0; i < SENSOR_COUNT; i++)  {
+      Serial.print(distances[i]);
+      Serial.print('\t');
+    }
+  }
+  Serial.println();
 
   for (uint8_t i = 0; i < SENSOR_COUNT; i++)  {
     if (Taps[i].get_gesture() > 0) {
@@ -152,4 +159,14 @@ void march(){
       
   }
 
+}
+
+void rainbowCycle(void){
+  const uint8_t hue = 8;
+  for (uint8_t i = 0; i < NUM_LEDS; i++) {
+      leds[i].r = hue*i*(rainbow_counter + i + 0  ) % 256;
+      leds[i].g = hue*i*(rainbow_counter + i + 64 ) % 256;
+      leds[i].b = hue*i*(rainbow_counter + i + 128) % 256;
+  }
+  ++rainbow_counter;
 }
